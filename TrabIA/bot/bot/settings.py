161104cd24015e02243560d9ab46f82204efd2aa
VScavoni <viniciusscavoni@gmail.com>
2018-06-24 16:24:31 -0300

    pipenv

--- TrabIA/bot/bot/settings.py
+++ TrabIA/bot/bot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')9b=#67weekxy5jiub(it7)q-td@#ya77qhs&yhg8n)wflx8+9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
